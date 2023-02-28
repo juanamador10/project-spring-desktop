4efc53f4be960a4ad014d862f22e103a5904b43a
juanamador10 <juan.amador.is@unipolidgo.edu.mx>
2023-02-28 13:39:29 -0600

    Version inicial del proyecto project-spring-desktop

---
+++ src/main/java/com/juanamador/projectspring/project_spring_desktop/App.java
@@ -0,0 +1,13 @@
+package com.juanamador.projectspring.project_spring_desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/juanamador/projectspring/project_spring_desktop/AppTest.java
@@ -0,0 +1,20 @@
+package com.juanamador.projectspring.project_spring_desktop;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
